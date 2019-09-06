7cc38f6252ddadf38a61cc51ba513fa93db0643c
Shinya Maeda <shinya@gitlab.com>
2019-09-06 23:18:26 +0700

    ok

--- lib/michi.rb
+++ lib/michi.rb
@@ -1,11 +1,26 @@
 require "michi/version"
+require 'yaml'
 
 module Michi
-  class Error < StandardError; end
+  class Command
+    MICHI_FILE_NAME = '.michi.yml'
 
-  class Test
-    def ho
-      puts "wan wan"
+    def execute
+      puts "config: #{config}"
+    end
+
+    private
+
+    def config
+      return @config if defined?(@config)
+
+      File.read(config_path).yield_self do |content|
+        @config = YAML.load(content)
+      end
+    end
+
+    def config_path
+      File.join(Dir.pwd, MICHI_FILE_NAME)
     end
   end
 end
